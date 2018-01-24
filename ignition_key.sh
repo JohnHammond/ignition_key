61ee821f008ed05cd5b62471a36f14d713a5b33a
John Hammond <johnhammond010@gmail.com>
2018-01-23 19:25:09 -0500

    Added install for task

--- ignition_key.sh
+++ ignition_key.sh
@@ -60,3 +60,6 @@ echo "" | sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
 sudo apt-get update
 sudo apt-get install -y simplescreenrecorder
 
+BLUE "Installing task..."
+sudo apt-get install -y taskwarrior
+
