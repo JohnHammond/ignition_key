c09e1948deb8be5919553ba2f640c8b86cfc7dff
John Hammond <johnhammond010@gmail.com>
2018-02-02 09:51:02 -0500

    Re-added ignition key...?

---
+++ ignition_key.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+# Define colors...
+RED=`tput bold && tput setaf 1`
+GREEN=`tput bold && tput setaf 2`
+YELLOW=`tput bold && tput setaf 3`
+BLUE=`tput bold && tput setaf 4`
+NC=`tput sgr0`
+
+function RED(){
+	echo -e "\n${RED}${1}${NC}"
+}
+function GREEN(){
+	echo -e "\n${GREEN}${1}${NC}"
+}
+function YELLOW(){
+	echo -e "\n${YELLOW}${1}${NC}"
+}
+function BLUE(){
+	echo -e "\n${BLUE}${1}${NC}"
+}
+
+# Testing if root...
+if [ $UID -ne 0 ]
+then
+	RED "You must run this script as root!" && echo
+	exit
+fi
+
+
+
+BLUE "Updating repositories..."
+sudo apt update
+
+BLUE "Installing git..."
+sudo apt install -y git
+
+BLUE "Installing Sublime Text..." # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
+
+BLUE "Installing terminator..."
+sudo apt install -y terminator
+
+BLUE "Setting terminator as the default terminal emulator..."
+sed -i s/Exec=gnome-terminal/Exec=terminator/g /usr/share/applications/gnome-terminal.desktop
+
+BLUE "Forcing a color prompt in ~/.bashrc..."
+grep "export PS1" ~/.bashrc
+if [ $? -eq 1 ]
+then
+	echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+fi
+
+BLUE "Installing SimpleScreenRecorder..."
+echo "" | sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
+sudo apt-get update
+sudo apt-get install -y simplescreenrecorder
+
+BLUE "Installing task..."
+sudo apt-get install -y taskwarrior
+
+BLUE "Installing pip..."
+sudo apt-get install -y python-pip
+
+BLUE "Removing boilerplate home directories..."
+rmdir ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
+
+BLUE "Installing guake..."
+sudo apt-get install -y guake
+
+BLUE "Installing openvpn..."
+sudo apt-get install -y openvpn
+
+BLUE "Installing nmap..."
+sudo apt-get install -y nmap
+
+BLUE "Installing docker..."
+sudo apt-get install -y docker.io
+sudo groupadd docker
+sudo usermod -aG docker `logname`
+
+BLUE "Installing curl..."
+sudo apt-get install -y curl
+
+BLUE "Installing pinta..."
+sudo apt-get install -y pinta
+
+BLUE "Installing exiftool..."
+sudo apt-get install -y exiftool
+
+BLUE "Installing Python PIL..."
+sudo apt-get install -y python-pil
+
+BLUE "Installing sqlitebrowser..."
+sudo apt-get install -y sqlitebrowser
+
+BLUE "Installing Wireshark..."
+sudo apt-get install -y wireshark
+
+BLUE "Install Real VNC Viewer..."
+wget "https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.17.1113-Linux-x64.deb" -O vnc_viewer.deb
+dpkg -i vnc_viewer.deb
+rm vnc_viewer.deb
+
+BLUE "Install Real VNC Connect (Server)..."
+wget 'https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.2.1-Linux-x64.deb' -O vnc_server.deb
+dpkg -i vnc_server.deb
+rm vnc_server.deb
+
+BLUE "Adding VNC Connect (Server) service to the default startup /etc/rc.local..."
+grep "vncserver-x11-serviced.service PS1" /etc/rc.local
+if [ $? -eq 1 ]
+then
+	echo "systemctl start vncserver-x11-serviced.service" >> ~/etc/rc.local
+fi
+
+BLUE "Installing Atom..."
+wget "https://atom.io/download/deb" -O atom.deb
+dpkg -i atom.deb
+rm atom.deb
+
+BLUE "Installing python-requests..."
+pip install requests
+
+BLUE "Installing idle..."
+sudo apt install -y idle
+
+BLUE "Installing xclip..."
+sudo apt install -y xclip
+grep "alias xclip" ~/.bashrc
+if [ $? -eq 1 ]
+then
+	echo "alias xclip='xclip -selection clipboard'" >> ~/.bashrc
+fi
+
+BLUE "Installing Python flask..."
+sudo pip install flask
+
+BLUE "Installing Python colorama..."
+sudo pip install colorama
+
+BLUE "Installing Python passlib..."
+sudo pip install passlib
+
+BLUE "Installing Spotify..."
+sudo snap install spotify
+
+BLUE "Installing Binwalk..."
+sudo apt install -y binwalk
+
+BLUE "Installing Tesseract..."
+sudo apt install -y tesseract-ocr
+
+
+BLUE "Installing Tesseract..."
+sudo apt install -y foremost
+
+
+BLUE "Installing rot13..."
+sudo apt install -y bsdgames	
